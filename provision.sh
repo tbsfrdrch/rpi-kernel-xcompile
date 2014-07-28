8fe1588acb5ed282719f444039b75fed3ffc7d16
Tobias Friedrich <tbsfrdrch@gmail.com>
2014-07-28 20:00:07 +0200

    Fix failing shell provisioning on first `vagrant up` call. Related: mitchellh/vagrant#896

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get -y install bc curl gcc-4.6-arm-linux-gnueabi make makeself ncurses-dev
+sudo apt-get -y install bc curl gcc-4.6-arm-linux-gnueabi make makeself ncurses-dev || true
 sudo ln -s /usr/bin/arm-linux-gnueabi-gcc-4.6 /usr/bin/arm-linux-gnueabi-gcc
 
